deee2cb20559ca3a6fe2929d44eb2ca0a1fec2a7
Nicholas Merritt <nimerritt@gmail.com>
2017-06-04 16:04:56 +0200

    fix: remove files completely

--- muesli-tools/build/src/parse.js
+++ muesli-tools/build/src/parse.js
@@ -1,9 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Parser = (function () {
-    function Parser() {
+var _ = require("lodash");
+var dom = require("dts-dom");
+function tsProperties(schema) {
+    return _.map(schema.properties, function (v, k) { return dom.create.property(k, tsType(v), schema.required.indexOf(k) !== -1 ?
+        dom.DeclarationFlags.None :
+        dom.DeclarationFlags.Optional); });
+}
+function tsType(schema) {
+    switch (schema.type) {
+        case 'string':
+            return dom.type.string;
+        case 'number':
+            return dom.type.number;
+        case 'array':
+            return dom.create.array(tsType(schema.items));
+        case 'object':
+            return dom.create.objectType(tsProperties(schema));
+        default:
+            var _exhaustiveCheck = schema;
+            return _exhaustiveCheck;
     }
-    Parser.prototype.parse = function (schema) {
-    };
-    return Parser;
-}());
+}
+function createInterface(name, root) {
+    var intf = dom.create.interface('Test');
+    intf.members = tsProperties(root);
+    return intf;
+}
+exports.createInterface = createInterface;
